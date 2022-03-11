d8887c8cca696878e4d390bbcfc6d4a46f79fecf
MRobot404 <qomc16@gmail.com>
2022-03-11 10:48:30 -0600

    Versión 2

--- src/main/java/com/mycompany/ejercicio1preparcial/MainClass.java
+++ src/main/java/com/mycompany/ejercicio1preparcial/MainClass.java
@@ -15,7 +15,7 @@ import rx.functions.Func2;
  */
 public class MainClass {
 
-    public static int precio, total, anterior = 0, actual,valor;
+    public static int precio, total, anterior = 0, actual, valor;
 
     public static void main(String[] args) {
         ArrayList<Consola> consolas = new ArrayList<>();
@@ -27,30 +27,27 @@ public class MainClass {
         consolas.add(new Consola("Internet", 10));
         consolas.add(new Consola("Internet", 11));
 
+        Observable observableconsola
+                = Observable
+                        .from(consolas.toArray())
+                        .map((result) -> {
+                            Consola consola = (Consola) result;
+                            return consola.getEdad();
+                        })
+                        .reduce(
+                                new Func2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer acumulador, Integer actual) {
+                                // System.out.println("Actual:"+actual);
+                                return acumulador + actual;
+                            }
+                        }
+                        );
+        observableconsola.subscribe((sumatoria) -> {
+            System.out.println(""
+                    + "Sumatoria: " + sumatoria);
+        });
 
-        for (int i = 0; i < consolas.size(); i++) {
-            precio = consolas.get(i).getEdad();
-            total = total + precio;
-        }
-        System.out.println("El total del programa " + total);
-        System.out.println("\n");
-        
-        valor=consolas.get(0).getEdad();
-        //Mostrar vector
-        for (int i = 0; i < consolas.size(); i++) {
-            System.out.println(consolas.get(i).getNombre()+" "+consolas.get(i).getEdad());
-        }
-        
-        for (int j = 0; j < consolas.size(); j++) {
-
-            if (consolas.get(j).getEdad() <=valor ) {
-
-            } else {
-            valor=consolas.get(j).getEdad();
-                
-            }
-        }
-        System.out.println("EL numero mayor de la lista es: " + valor);
     }
 
 }
